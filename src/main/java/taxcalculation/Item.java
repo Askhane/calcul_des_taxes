7662c426f636d28537f2753e45be6fa264e592f6
Jasmine Duchon <jasmine.duchon@arolla.fr>
2025-05-14 15:45:21 +0200

    Still Red

--- src/main/java/taxcalculation/Item.java
+++ src/main/java/taxcalculation/Item.java
@@ -1,5 +1,8 @@
 package taxcalculation;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 class Item {
     public static final double TAX_RATE = 0.1;
     private final double price;
@@ -11,7 +14,9 @@ class Item {
     }
 
     public double taxAmount() {
-        return price * TAX_RATE;
+        double rawAmount = price * TAX_RATE;
+        double roundedValueTimes2 = BigDecimal.valueOf(rawAmount * 2).setScale(1, RoundingMode.UP).doubleValue();
+        return roundedValueTimes2 / 2;
     }
 
     public double taxedPrice() {
