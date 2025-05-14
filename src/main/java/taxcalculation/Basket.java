d11289a64770957bc63f5e8c60cc80eb9049ebfa
Jasmine Duchon <jasmine.duchon@arolla.fr>
2025-05-14 14:45:16 +0200

    Refactor

--- src/main/java/taxcalculation/Basket.java
+++ src/main/java/taxcalculation/Basket.java
@@ -4,10 +4,20 @@ public class Basket {
     private boolean hasItem = false;
 
     public String receipt() {
+        StringBuilder receipt = new StringBuilder();
         if (hasItem) {
-            return "1 CD musical : 16.49 Montant des taxes : 1.50 Total : 16.49";
+            receipt.append("1 CD musical : 16.49 ");
         }
-        return "Montant des taxes : 0.00 Total : 0.00";
+        receipt.append("Montant des taxes : ").append(taxAmount()).append(" Total : ").append(totalAmount());
+        return receipt.toString();
+    }
+
+    private String totalAmount() {
+        return hasItem ? "16.49" : "0.00";
+    }
+
+    private String taxAmount() {
+        return hasItem ? "1.50" : "0.00";
     }
 
     public Basket addItem(String itemDescription) {
