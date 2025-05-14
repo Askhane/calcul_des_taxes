1abaa1e7a5301a44f80e5745e337c2424f580e6d
Jasmine Duchon <jasmine.duchon@arolla.fr>
2025-05-14 14:59:43 +0200

    Green

--- src/main/java/taxcalculation/Basket.java
+++ src/main/java/taxcalculation/Basket.java
@@ -1,26 +1,35 @@
 package taxcalculation;
 
+import java.util.Locale;
+
 public class Basket {
     private boolean hasItem = false;
+
+    private String itemPrice = "0.00";
 
     public String receipt() {
         StringBuilder receipt = new StringBuilder();
         if (hasItem) {
-            receipt.append("1 CD musical : 16.49 ");
+            receipt.append("1 CD musical : ").append(taxedPrice()).append(" ");
         }
         receipt.append("Montant des taxes : ").append(taxAmount()).append(" Total : ").append(totalAmount());
         return receipt.toString();
     }
 
+    private String taxedPrice() {
+        return String.format(Locale.US, "%.2f", Double.parseDouble(itemPrice) * 1.1);
+    }
+
     private String totalAmount() {
-        return hasItem ? "16.49" : "0.00";
+        return taxedPrice();
     }
 
     private String taxAmount() {
-        return hasItem ? "1.50" : "0.00";
+        return String.format(Locale.US, "%.2f", Double.parseDouble(itemPrice) * 0.1);
     }
 
     public Basket addItem(String itemDescription) {
+        itemPrice = itemDescription.substring(itemDescription.indexOf(" à ") + 3);
         hasItem = true;
         return this;
     }
